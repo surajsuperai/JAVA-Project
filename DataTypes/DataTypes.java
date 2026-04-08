12d8d53a8fc9e1e4fe53b7ecce816688365db9ed
surajsuperai <er.srk123@gmail.com>
2026-04-08 21:57:05 +0530

    Added DataTypes folder correctly

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int num = 10;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println(num);
+        System.out.println(price);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
+    }
+}
