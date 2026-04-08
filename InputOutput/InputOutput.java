ffb601ee20640840e2cf7a0d43bab23dc12f3d22
surajsuperai <er.srk123@gmail.com>
2026-04-08 22:13:09 +0530

    Added InputOutput folder correctly

---
+++ InputOutput/InputOutput.java
@@ -0,0 +1,27 @@
+package InputOutput;
+
+import java.util.Scanner;
+public class InputOutput {
+
+
+    public static void main(String[] args) {
+        // Create Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+
+        // Input a string
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Input a number
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        // Output the values
+        System.out.println("Your name is: " + name);
+        System.out.println("Your age is: " + age);
+
+        // Close scanner
+        scanner.close();
+    
+}
+}
