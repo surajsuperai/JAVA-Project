816b5a688de83b6128548a9c2fc9f214fb70d731
surajsuperai <er.srk123@gmail.com>
2026-04-09 23:19:47 +0530

    Fixed folder naming to lowercase

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,5 +1,5 @@
 package demo;
-class Demo {
+ public class Demo {
     public static void main(String[] args) {
         System.out.println("Suraj Kumar");
         System.out.println("Hello Github");
